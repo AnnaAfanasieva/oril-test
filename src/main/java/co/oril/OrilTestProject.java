7dd65e89f0ea260e4c7bdf76d11e65b9226b34da
AnnaAfanasieva <aniafanasieva24@gmail.com>
2022-03-16 00:28:50 +0100

    create module, add connection with DB

---
+++ src/main/java/co/oril/OrilTestProject.java
@@ -0,0 +1,13 @@
+package co.oril;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrilTestProject {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrilTestProject.class, args);
+    }
+
+}
---
+++ src/test/java/co/oril/OrilTestProjectTests.java
@@ -0,0 +1,13 @@
+package co.oril;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrilTestProjectTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
